0835ce6c8d7beda6a7e155718ae96c4b66e0e2c5
kthn2022 <98634983+kthn2022@users.noreply.github.com>
2022-03-13 18:10:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ function myFunction(x) {
   else hidden.style.display = 'block';
   
   var main = document.getElementById('mbody');
-   if (main.style.marginLeft == '220px') main.style.marginLeft='0px';
-  else main.style.marginLeft='220px';
+   if (main.style.marginLeft == '235px') main.style.marginLeft='0px';
+  else main.style.marginLeft='235px';
+}
+function Function() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("menu");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
